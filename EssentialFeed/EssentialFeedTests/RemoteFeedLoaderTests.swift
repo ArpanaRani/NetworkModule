9934fd8e399acc6cafc7f586a76c672dbd19c915
ArpanaRani <arpanaaims@gmail.com>
2024-11-09 20:50:20 +0530

    Refactored code , removed duplicated code

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -23,13 +23,7 @@ protocol HTTPClient {
     
 }
 
-class HTTPClientSpy : HTTPClient {
-    
-    var requestedURL: URL?
-     func get(from url :URL){
-        requestedURL =  url
-    }
-}
+
 
 class RemoteFeedLoader{
     
@@ -51,7 +45,7 @@ class RemoteFeedLoader{
     func test_init_doesNotRequestDataFromURL(){
         let url = URL(string: "https://a-url.com")!
 
-        let client = HTTPClientSpy()
+        let (_ , client) =  makeSUT()
         _ = RemoteFeedLoader(url: url, client: client)
        // let client = HTTPClient.shared -> As Spy class created use that
        
@@ -61,10 +55,23 @@ class RemoteFeedLoader{
      
      func test_init_load_RequestDataFromURL(){
          let url = URL(string: "https://a-url.com")!
-         let client = HTTPClientSpy()
-       //  HTTPClient.shared = client ->  as we inject client as proper remove this line
-         let sut = RemoteFeedLoader(url: url  , client: client)
+         let ( sut, client) = makeSUT(url:  url)
          sut.load()
          XCTAssertEqual(url , client.requestedURL)
+     }
+     
+     private func makeSUT(url: URL = URL(string: "https://a-url.com")! ) -> (sut: RemoteFeedLoader , client: HTTPClientSpy){
+         
+         let client = HTTPClientSpy()
+         let sut = RemoteFeedLoader(url: url, client: client)
+         return(sut, client)
+     }
+     
+     private class HTTPClientSpy : HTTPClient {
+         
+         var requestedURL: URL?
+          func get(from url :URL){
+             requestedURL =  url
+         }
      }
 }
