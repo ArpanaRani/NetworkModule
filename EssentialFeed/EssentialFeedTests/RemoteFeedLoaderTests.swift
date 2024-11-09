708ec0e10e4732fbf074741183b657bbd0aa4e71
ArpanaRani <arpanaaims@gmail.com>
2024-11-09 20:23:59 +0530

    Changed HttpClient to protocol

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -7,10 +7,10 @@
 
 import XCTest
 
-class HTTPClient {
+protocol HTTPClient {
     
-    static var shared = HTTPClient()
-  //  private init() {} -> AS we changes let to var fro shared , we don't need this method
+ //   static var shared = HTTPClient()
+  //  private init() {} -> As we changes let to var fro shared , we don't need this method
     
     //  Move the below comment code to a subclass as The same Http class is used in production , so we don not want set a property, just invoke a method
     /*
@@ -19,41 +19,48 @@ class HTTPClient {
      requestedURL = URL(string: url)
      } */
     
-    func get(from url :String){}
+    func get(from url :String)
     
 }
 
 class HTTPClientSpy : HTTPClient {
     
     var requestedURL: URL?
-    override func get(from url :String){
+     func get(from url :String){
         requestedURL = URL(string: url)
     }
 }
 
 class RemoteFeedLoader{
     
+    var client : HTTPClient
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
     func load(){
-        HTTPClient.shared.get(from: "https://www.apple.com")
+      //  HTTPClient.shared.get(from: "https://www.apple.com") -> This way we are allocating space to memory
+        //instaed of this we can inject the URL
+        client.get(from: "https://www.apple.com")
     }
 }
  class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL(){
         
-        _ = RemoteFeedLoader()
-       // let client = HTTPClient.shared -> As Spy class created use that
         let client = HTTPClientSpy()
-        HTTPClient.shared = client
+        _ = RemoteFeedLoader(client: client)
+       // let client = HTTPClient.shared -> As Spy class created use that
+       
+        //  HTTPClient.shared = client ->  as we inject client as proper remove this line
         XCTAssertNil(client.requestedURL)
     }
      
      func test_init_load_RequestDataFromURL(){
          
          let client = HTTPClientSpy()
-         HTTPClient.shared = client
-
-         let sut = RemoteFeedLoader()
+       //  HTTPClient.shared = client ->  as we inject client as proper remove this line
+         let sut = RemoteFeedLoader(client: client)
          sut.load()
          XCTAssertNotNil(client.requestedURL)
      }
